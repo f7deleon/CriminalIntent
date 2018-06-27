0a53ec7c089eb3ec6466e7f161f5dffca893a771
f7deleon <felipe@xmartlabs.com>
2018-06-27 15:58:34 -0300

    Fixed some bugs and added detekt

--- app/src/main/java/com/example/felipe/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/felipe/criminalintent/CrimeFragment.kt
@@ -10,23 +10,20 @@ import android.view.ViewGroup
 import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_crime.view.*
 
-
 class CrimeFragment : Fragment() {
     private var crime = Crime()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         var view = inflater.inflate(R.layout.fragment_crime, container, false)
         initViews(view)
-
-    return view
-}
+        return view
+    }
 
     private fun initViews(view: View) {
         with(view) {
             chkSolved.setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
             }
-
             btnCrime.text = crime.date.toString()
             titleEditText.afterTextChangeListener {
                 crime.title = it.toString()
@@ -35,12 +32,12 @@ class CrimeFragment : Fragment() {
     }
 
     private fun EditText.afterTextChangeListener(
-        afterTextChange: ((s: Editable) -> Unit)) = addTextChangedListener(object : TextWatcher {
+            afterTextChange: ((s: Editable) -> Unit)) = addTextChangedListener(object : TextWatcher {
 
-    override fun afterTextChanged(s: Editable) = afterTextChange(s)
+        override fun afterTextChanged(s: Editable) = afterTextChange(s)
 
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-})
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
 }
