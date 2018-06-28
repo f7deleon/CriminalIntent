c7ed95c6d8388dd2c9c4a4e446a9017f10df6959
f7deleon <felipe@xmartlabs.com>
2018-06-28 15:43:57 -0300

    fixed crime holder comments

--- app/src/main/java/com/example/felipe/criminalintent/CrimeAdapter.kt
+++ app/src/main/java/com/example/felipe/criminalintent/CrimeAdapter.kt
@@ -8,18 +8,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.list_item_crime.view.*
 
-class CrimeAdapter(val crimes: List<Crime>, var activity: FragmentActivity?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+class CrimeAdapter(val crimes: List<Crime>, var activity: FragmentActivity?) : RecyclerView.Adapter<CrimeHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val layoutInflater = LayoutInflater.from(activity)
         return when (viewType) {
-            ViewType.POLICE.ordinal -> CrimeHolderPolice(layoutInflater, parent)
-            else -> CrimeHolder(layoutInflater, parent)
+            ViewType.POLICE.ordinal -> CrimeHolderPolice(layoutInflater, parent, activity)
+            else -> CrimeHolderNormal(layoutInflater, parent, activity)
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as? CrimeHolder)?.bind(crimes[position])
-        (holder as? CrimeHolderPolice)?.bind(crimes[position])
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        holder.bind(crimes[position])
     }
 
     override fun getItemViewType(position: Int): Int = when (crimes[position].isPoliceRequire) {
@@ -28,51 +27,36 @@ class CrimeAdapter(val crimes: List<Crime>, var activity: FragmentActivity?) : R
     }
 
     override fun getItemCount() = crimes.size
-
-    private inner class CrimeHolder(inflater: LayoutInflater, parent: ViewGroup) :
-            RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item_crime, parent, false)),
-            View.OnClickListener {
-
-        private var crime: Crime? = null
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        fun bind(crime: Crime) {
-            this.crime = crime
-            itemView.txtCrimeTitle.text = crime.title
-            itemView.txtCrimeDate.text = crime.date.toString()
-        }
-
-        override fun onClick(view: View) {
-            Toast.makeText(activity, "${crime?.title} clicked!", Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
-    private inner class CrimeHolderPolice(inflater: LayoutInflater, parent: ViewGroup) :
-            RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item_crime_police, parent, false)),
-            View.OnClickListener {
-
-        private var crime: Crime? = null
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        fun bind(crime: Crime) {
-            this.crime = crime
-            itemView.txtCrimeTitle.text = crime.title
-            itemView.txtCrimeDate.text = crime.date.toString()
-        }
-
-        override fun onClick(view: View) {
-            Toast.makeText(activity, "${crime?.title} clicked!", Toast.LENGTH_SHORT).show()
-        }
-    }
 }
 
 private enum class ViewType {
     NORMAL, POLICE
 }
+
+open class CrimeHolder(inflater: LayoutInflater, parent: ViewGroup, layout: Int, var activity: FragmentActivity?) :
+        RecyclerView.ViewHolder(inflater.inflate(layout, parent, false)),
+        View.OnClickListener {
+
+    private var crime: Crime? = null
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    fun bind(crime: Crime) {
+        this.crime = crime
+        itemView.txtCrimeTitle.text = crime.title
+        itemView.txtCrimeDate.text = crime.date.toString()
+    }
+
+    override fun onClick(view: View) {
+        Toast.makeText(this@CrimeHolder.activity, "${crime?.title} clicked!", Toast.LENGTH_SHORT).show()
+    }
+
+}
+
+private class CrimeHolderPolice(inflater: LayoutInflater, parent: ViewGroup, activity: FragmentActivity?) :
+        CrimeHolder(inflater, parent, R.layout.list_item_crime_police, activity)
+
+private class CrimeHolderNormal(inflater: LayoutInflater, parent: ViewGroup, activity: FragmentActivity?) :
+        CrimeHolder(inflater, parent, R.layout.list_item_crime, activity)
